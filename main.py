b098a968f9e60c0f3f83d45250834f2de4a33f7c
Gabriel Reliquias <gabrielreliquias@hotmail.com>
2026-06-19 04:35:24 -0300

    refactor: separar responsabilidades do main.py

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from services.supabase_service import buscar_contatos
 from services.zapi_service import enviar_mensagem
 
+def gerar_mensagem(nome):
+    return f"Olá, {nome} tudo bem com você?"
+
+def processar_contato(contato):
+    telefone = contato["telefone"]
+    nome = contato["nome"]
+    
+    mensagem = gerar_mensagem(nome)
+
+    print(f"\nEnviando mensagem para {nome}...")
+    enviar_mensagem(telefone, mensagem)
+
 def main():
     contatos = buscar_contatos()
 
     for contato in contatos:
-        telefone = contato["telefone"]
-        nome = contato["nome"]
+        processar_contato(contato)
 
-        mensagem = f"Olá, {nome} tudo bem com você?"
-
-        print(f"\nEnviando mensagem para {nome}...")
-        enviar_mensagem(telefone, mensagem)
 
 main()
